ebe24be319e9800646971ede550f2c268ec729f2
Anmol chauhan <anmolchauhan402@gmail.com>
2025-01-15 10:56:43 +0530

    done

---
+++ sorting/insertion_sort.cpp
@@ -0,0 +1,24 @@
+        #include <iostream>
+using namespace std;
+int main(){
+    int swapp = 0;
+    int arr[]={5,4,2,5,4,52,5,1,5,2,5,12,5,1,5,2,54,48,4,582,5,15,52,65,5,3,1};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    for (int i = 0; i < n; i++)
+    {int key = arr[i];
+    int j = i-1;
+      while (j>=0&&arr[j]>key)
+      {
+        arr[j+1]=arr[j];
+        j--;
+      }
+      arr[j+1]=key;
+   }
+  
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arr[i]<<" "<< endl;
+    }
+    
+    return 0;
+}
